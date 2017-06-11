1f66ad08d39754627de29a1118ddbb951ba54ed8
readlnh <readlnh@163.com>
2017-06-11 23:33:53 +0800

    port

--- prot.cpp
+++ prot.cpp
@@ -1,3 +1,26 @@
 Port::Port(uint16_t portnumber) {
-    this->portnumber
+    this->portnumber = portnumber;
 }
+
+Port::~Port() {
+
+}
+
+Port8Bit::Port8Bit(uint16_t portnumber) : Port(portnumber) {
+
+}
+
+Port8Bit::~Port8Bit() {
+
+}
+
+void Port8Bit::Write(uint8_t data) {
+    _asm_volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+}
+
+uint8_t Port8Bit::Read() {
+    uint8_t result;
+    _asm_volatile();
+    return 
+}
+
