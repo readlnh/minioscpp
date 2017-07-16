ff1c04ad5d1531c4b017cb902fd528334ebb0ddb
readlnh <readlnh@163.com>
2017-07-16 15:47:57 +0800

    printf

--- kernel.cpp
+++ kernel.cpp
@@ -3,8 +3,31 @@
 
 void printf(char* str) {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-    for(int i = 0; str[i] != '\0'; i++) 
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    
+    static uint8_t x, y;
+
+    for(int i = 0; str[i] != '\0'; i++) { 
+        if(str[i] == '\n') {
+            y++;
+            x = 0;
+        }
+        else {
+            VideoMemory[80*y + x] = (VideoMemory[80*y + x] & 0xFF00) | str[i];
+            x++;
+        }
+
+        if(x >= 80) {
+            y++;
+            x = 0;
+        }
+
+        if(y >= 25) {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    (VideoMemory[80*y + x] & 0xFF00) | ' ';
+            x = y = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();
@@ -16,8 +39,9 @@ extern "C" void callConstructors() {
 }
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t magicnumber) {
-    printf("Hello World!");
-    printf("dfd");
+    printf("Hello World!\n");
+    printf("dfd\n");
+    printf("hhhhhhhh");
     GlobalDescriptorTable gdt;
     while(1);
 }
