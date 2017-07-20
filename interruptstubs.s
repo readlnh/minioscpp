1864aa3d1f81dd6a92560e939e9b654c6ed9ea63
readlnh <readlnh@163.com>
2017-07-20 22:49:26 +0800

    interrupt

---
+++ interruptstubs.s
@@ -0,0 +1,45 @@
+.set IRQ_BASE, 0x20
+
+.section text
+
+.extern _ZN16InterruptManager15handleInterruptEhj
+
+.macro HandleException num
+.global _ZN16InterruptManager16handleException\num\()Ev
+    movb $\num,(interruptnumber)
+    jmp int_bottom
+.endm
+
+.macro HandleInterruptRequest num
+.global _ZN16InterruptManager15handleInterruptRequest\num\()Ev
+    movb $\num + IRQ_BASE, (interruptnumber)
+    jmp int_bottom
+.endm
+
+HandleInterruptRequest 0x00
+HandleInterruptRequest 0x01
+
+int_bottom:
+
+    pusha
+    pushl %ds
+    pushl %es
+    pushl %fs
+    pushl %gs
+
+    pushl %esp
+    push (interruptnumber)
+    call _ZN16InterruptManager15handleInterruptEhj
+    # addl $5, %esp
+    movl %eax, %esp
+
+    popl %ds
+    popl %es
+    popl %fs
+    popl %gs
+    popa
+
+    iret
+
+.data
+    interruptnumber: .byte 0
